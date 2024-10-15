3c06a0d8f93e2eb2a21f7e45f54f66e28174616b
Emile Drablant <emiledrablant@proton.me>
2024-10-15 15:09:23 -0300

    paint a red trail on a 48*48 grid

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function createGrid(size) {
     const container = document.querySelector("#container");
     for (let i = 1; i <= size; i++) {
-        let squareDiv = document.createElement("div");
-        squareDiv.classList.add("square");
-        container.appendChild(squareDiv);
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 1; j <= size; j++) {
+            let squareDiv = document.createElement("div");
+            squareDiv.classList.add("square");
+            row.appendChild(squareDiv);
+
+            squareDiv.addEventListener("mouseenter", () => {
+                squareDiv.style.backgroundColor = "red";
+            });
+        }
+        container.appendChild(row);
     }
 }
 
-createGrid(4);
+createGrid(48);
