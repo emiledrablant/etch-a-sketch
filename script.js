47146cd9337544b16c8a9c0778e4137746c74010
Emile Drablant <emiledrablant@proton.me>
2024-10-15 15:45:54 -0300

    generate a random colour for each square

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const container = document.querySelector("#container");
+
 function createGrid(size) {
-    const container = document.querySelector("#container");
     for (let i = 1; i <= size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -9,11 +10,36 @@ function createGrid(size) {
             row.appendChild(squareDiv);
 
             squareDiv.addEventListener("mouseenter", () => {
-                squareDiv.style.backgroundColor = "red";
+                squareDiv.style.backgroundColor = generateColour();
             });
         }
         container.appendChild(row);
     }
 }
 
-createGrid(48);
+function randomBetween(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+function generateColour() {
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgb(${r},${g},${b})`;
+}
+
+function selectSize() {
+    let newSize = parseInt(prompt("Please enter a grid size between 1 and 100."));
+    if (newSize >= 1 && newSize <= 100) {
+        removeGrid();
+        createGrid(newSize);
+    }
+}
+
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+createGrid(16);
